1c70a365bcd1b5171025c9e2dd30ce5ba38a9929
ChiLei01 <chi.lei@student.howest.be>
2018-04-23 09:37:32 +0200

    Add project files.

---
+++ MijnFilms_LeiChiMei/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MijnFilms_LeiChiMei.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
